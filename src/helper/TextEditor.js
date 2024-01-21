cfc02916e821666a28cfc645f5df36bc0d63e75b
sandeepigsrs <sandeep.ghuraiya@igsrs.com>
2024-01-22 03:39:55 +0530

    Update TextEditor.js

--- src/helper/TextEditor.js
+++ src/helper/TextEditor.js
@@ -3,6 +3,7 @@ import { EditorState, convertToRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import draftToHtml from "draftjs-to-html";
 import htmlToDraft from "html-to-draftjs";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 const TextEditor = () => {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
