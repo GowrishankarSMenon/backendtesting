3c85527abd26ed702b4c6007fc8204ea9dbf99d3
i0dheerajdileep <i.dheerajdileep@gmail.com>
2024-07-15 12:27:53 +0530

    feat:integrated tailwind css

--- src/components/bannerTitle.js
+++ src/components/bannerTitle.js
@@ -4,7 +4,7 @@ import {Box, Heading } from "@chakra-ui/react"
 const BannerTitle = () => {
   return (
     <Box className='text-main-heading text--wrapper'>
-      <Heading as={"h1"} textAlign={'left'} fontSize={'48px'} fontWeight={'600'} lineHeight={"58px"} fontFamily={'Inter'}>
+      <Heading as={"h1"} textAlign={'left'} fontSize={'48px'} fontWeight={'600'} lineHeight={"58px"} fontFamily={'Inter'} >
         Get your dream job with Alchemus
       </Heading>
     </Box>
