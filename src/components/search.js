8885b8390989811d18ebc40c0938617808247586
dhanush <dhanushpk50@gmail.com>
2024-07-18 17:23:27 +0530

    fix:Job search drop down

--- src/components/search.js
+++ src/components/search.js
@@ -1,109 +1,89 @@
-import React from "react";
-import { Box, Flex, Input, Select, Button } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, Flex, Input, Menu, MenuButton, MenuList, MenuItem, Button, MenuDivider } from "@chakra-ui/react";
 import { select_sector } from "../selectData/selectOption";
 import { select_country } from "../selectData/selectCountry";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+
+const ScrollMenuList = ({ items, onItemClick }) => (
+  <MenuList
+    maxHeight="300px"
+    overflowY="scroll"
+    sx={{
+      '&::-webkit-scrollbar': {
+        width: '8px',
+      },
+      '&::-webkit-scrollbar-track': {
+        background: '#f1f1f1',
+      },
+      '&::-webkit-scrollbar-thumb': {
+        background: '#888',
+        borderRadius: '8px',
+      },
+      '&::-webkit-scrollbar-thumb:hover': {
+        background: '#555',
+      },
+    }}
+  >
+    {items.map((item, i) => (
+      <>
+        <MenuItem key={i} onClick={() => onItemClick(item)}>
+          {item}
+        </MenuItem>
+        <MenuDivider />
+      </>
+    ))}
+  </MenuList>
+);
 
 const SearchSection = () => {
-  // console.log(select_sector);
+  const [selectedJobOption, setSelectedJobOption] = useState(null);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
   return (
     <Box className="search-bar-container">
       <Box className="search-bar-box">
         <Box className="search-bar-layout">
-          <Flex
-            align="center"
-            justify="space-between"
-            className="inner-search-wrapper"
-          >
-            <Box className="" w={"35%"} maxWidth={"310px"}>
-              <Input placeholder="Enter Skills/Designations/compaines" />
+          <Flex align="center" justify="space-between" className="inner-search-wrapper">
+            <Box w="33%" maxWidth="300px">
+              <Input placeholder="Enter Skills/Designations/Companies" />
             </Box>
-            <Box w={{ base: "100%", sm: "50%", md: "35%", lg: "22%" }} maxWidth="215px" m="0 auto">
-              <Select
-                placeholder="Job Category"
-                borderColor="gray.300"
-                borderBottom="1px solid #e2e8f0"
-                _hover={{ borderColor: "gray.400" }}
-                _focus={{ borderColor: "gray.500",boxShadow: "0 0 0 1px gray.500 !important"  }}
-                bg="white"
-                color="gray.700"
-                size="md"
-                sx={{
-                  "::-webkit-scrollbar": {
-                    width: "8px",
-                  },
-                  "::-webkit-scrollbar-track": {
-                    background: "#f1f1f1",
-                  },
-                  "::-webkit-scrollbar-thumb": {
-                    background: "#888",
-                    borderRadius: "8px",
-                  },
-                  "::-webkit-scrollbar-thumb:hover": {
-                    background: "#555",
-                  },
-                  option: {
-                    fontSize: "14px",
-                    fontWeight: "500",
-                    minHeight: "32px  !important",
-                    lineHeight: "32px  !important", // Adjust the line height if needed
-                    padding: "8px 12px  !important", // Adjust padding as necessary
-                  },
-                  "option:first-of-type": {
-                    color: "gray",
-                  },
-                }}
-              >
-                {select_sector.map((select, i) => (
-                  <option key={i} value={select.toLowerCase()}
-                  >
-                    {select}
-                  </option>
-                ))}
-              </Select>
+            <Box w="22%" maxWidth="215px">
+              <Menu>
+                <MenuButton
+                  px={4}
+                  py={2}
+                  w="100%"
+                  borderRadius="md"
+                  borderWidth="1px"
+                  transition="all 0.2s"
+                  _hover={{ bg: 'gray.400' }}
+                  _expanded={{ bg: 'blue.400' }}
+                  _focus={{ boxShadow: 'outline' }}
+                >
+                  {selectedJobOption || <>Job Category <ChevronDownIcon ml={6} /></>}
+                </MenuButton>
+                <ScrollMenuList items={select_sector} onItemClick={setSelectedJobOption} />
+              </Menu>
             </Box>
-            <Box className="" w={"30%"} maxWidth={"300px"}>
-              <Select
-                placeholder="Select country"
-                borderColor="gray.300"
-                _hover={{ borderColor: "gray.400" }}
-                _focus={{ borderColor: "gray.500", boxShadow: "0 0 0 1px gray.500" }}
-                bg="white"
-                color="gray.700"
-                size="md"
-                sx={{
-                  "::-webkit-scrollbar": {
-                    width: "8px",
-                  },
-                  "::-webkit-scrollbar-track": {
-                    background: "#f1f1f1",
-                  },
-                  "::-webkit-scrollbar-thumb": {
-                    background: "#888",
-                    borderRadius: "8px",
-                  },
-                  "::-webkit-scrollbar-thumb:hover": {
-                    background: "#555",
-                  },
-                  option: {
-                    fontSize: "14px",
-                    fontWeight: "500",
-                  },
-                  "option:first-of-type": {
-                    color: "gray",
-
-                  },
-                }}
-              >
-                {select_country.map((select, i) => {
-                  return (
-                    <option key={i} value={select.code} style={{ padding: "12px", margin: "12px" }}>
-                      {select.country}
-                    </option>
-                  );
-                })}
-              </Select>
+            <Box w="30%" maxWidth="300px">
+              <Menu>
+                <MenuButton
+                  px={4}
+                  py={2}
+                  w="100%"
+                  borderRadius="md"
+                  borderWidth="1px"
+                  transition="all 0.2s"
+                  _hover={{ bg: 'gray.400' }}
+                  _expanded={{ bg: 'blue.400' }}
+                  _focus={{ boxShadow: 'outline' }}
+                >
+                  {selectedCountry || <>Select country <ChevronDownIcon ml={16} /></>}
+                </MenuButton>
+                <ScrollMenuList items={select_country.map(c => c.country)} onItemClick={setSelectedCountry} />
+              </Menu>
             </Box>
-            <Box className="button button-wrapper" w={"15%"} maxWidth={"170px"} ml={"1rem"}>
+            <Box w="15%" maxWidth="170px">
               <Button size="md" className="search-btn">
                 Search
               </Button>
